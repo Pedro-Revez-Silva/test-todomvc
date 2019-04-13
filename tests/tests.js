5ef73a6a769bd334a78e36531316c50db24e1152
Pedro Silva <pedro.silva@lovelystay.com>
2019-04-13 09:27:25 +0100

    first commit

---
+++ tests/tests.js
@@ -0,0 +1,20 @@
+var config = require('../nightwatch.conf.js');
+
+module.exports = {
+  '#main and #footer should be hidden when there are no todos': function(browser) {
+    browser
+      .url('https://vast-taiga-34449.herokuapp.com/index.html')
+      .waitForElementVisible('body')
+      .assert.attributeEquals('section[class=main]', 'style', 'display: none;')
+      .assert.attributeEquals('footer[class=footer]', 'style', 'display: none;')
+      .saveScreenshot('guinea-pig-test.png');
+  },
+
+  'Add new todo': function(browser) {
+    browser
+      .assert.attributeEquals('section[class=main]', 'style', 'display: none;')
+      .assert.attributeEquals('footer[class=footer]', 'style', 'display: none;')
+      .saveScreenshot('guinea-pig-test.png')
+      .end();
+  }
+};
