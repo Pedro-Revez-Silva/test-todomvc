a10986d09158219d24a864e85449543912b03a6d
Pedro Silva <pedro.silva@lovelystay.com>
2019-04-14 12:50:40 +0100

    add more test cases

--- tests/tests.js
+++ tests/tests.js
@@ -10,10 +10,59 @@ module.exports = {
       .assert.attributeEquals('footer[class=footer]', 'style', 'display: none;')
   },
 
-  'Add new todo': function(browser) {
+  'Add new todos': function(browser) {
     browser
-      .assert.attributeEquals('section[class=main]', 'style', 'display: none;')
-      .assert.attributeEquals('footer[class=footer]', 'style', 'display: none;')
-      .end();
+      .setValue('input[class=new-todo]', 'Task1')
+      .keys(browser.Keys.ENTER)
+      .assert.containsText('.todo-list li:first-child label', 'Task1')
+      .setValue('input[class=new-todo]', 'Task2')
+      .keys(browser.Keys.ENTER)
+      .assert.containsText('.todo-list li:nth-child(2) label', 'Task2')
+      .setValue('input[class=new-todo]', 'Task3')
+      .keys(browser.Keys.ENTER)
+      .assert.containsText('.todo-list li:nth-child(3) label', 'Task3');
+  },
+
+  'Show how many items are left to complete': function(browser){
+    browser
+      .assert.containsText('.todo-count', '3 items left');
+  },
+
+  'Mark one as completed': function(browser){
+    browser
+      .click('.todo-list li:first-child .toggle')
+      .assert.cssClassPresent('.todo-list li:first-child', 'completed');
+  },
+
+  'Show how many items are left to complete after checking one as completed': function(browser){
+    browser
+      .assert.containsText('.todo-count', '2 items left');
+  },
+
+  'Clear completed tasks': function(browser){
+    browser
+      .click('.todo-list li:nth-child(2) .toggle')
+      .assert.cssClassPresent('.todo-list li:nth-child(2)', 'completed')
+      .click('.todo-list li:nth-child(3) .toggle')
+      .assert.cssClassPresent('.todo-list li:nth-child(3)', 'completed')
+      .click('.clear-completed')
+      .pause();
   }
+
+  // I was unable to test this. The input field is set as element not interactable. Even simulating a double click the setValue didn't work
+  /*
+  'Edit todo list item': function(browser){
+   browser 
+    .setValue('.todo-list li:nth-child(2) .edit', 'Edited Task2')
+    .keys(browser.Keys.ENTER)
+    .assert.containsText('.todo-list li:nth-child(2) label', 'Edited Task2');
+  } */
+
+  // This feature is not available on the backbone implementation of todomvc
+  /*
+  'Mark all as completed': function(browser){
+
+  } */
+
+
 };
